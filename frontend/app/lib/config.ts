3d6e6f513fbacc523fdd8a6cba9b8ca52624edb3
kiko <kiko.binary@gmail.com>
2026-02-22 18:20:41 +0500

    hotfix: apply frontend changes

--- frontend/app/lib/config.ts
+++ frontend/app/lib/config.ts
@@ -10,7 +10,7 @@ export const API_BASE_URL: string =
     (
         process.env.NEXT_PUBLIC_API_BASE ||
         process.env.NEXT_PUBLIC_API_URL ||
-        'http://127.0.0.1:8000'
+        'http://127.0.0.1:8080'
     ).replace(/\/$/, '');
 
 /**
